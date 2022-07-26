48d5978cfd345a06a0934ba7abb584ef2845af1e
DKiff <110047464+DannKifa@users.noreply.github.com>
2022-07-26 21:00:24 +0300

    Conditional operator

--- python1.py
+++ python1.py
@@ -1,1 +1,19 @@
 #
+check_true = True
+check_false = False
+print(check_true)
+print(check_false)
+print(1 == 1)
+print(1 != 1)
+print(1 > 0)
+print(1 >= 1)
+print(True == False)
+print(1 == 1.0)
+
+if 1 == 1:
+    print('Hello, word!')
+elif 0 == 2:
+    print('Cool')
+else:
+    print("Goodbye, word!")
+# and, or, not - operators
