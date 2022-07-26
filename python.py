e3489e7bce7a5decdb70ab7e2aec376761093024
DKiff <110047464+DannKifa@users.noreply.github.com>
2022-07-26 20:16:53 +0300

    Create variable.

--- python.py
+++ python.py
@@ -1,1 +1,31 @@
-#
+# - this comment
+print("Hello, word!")
+
+# ** - exponentiation
+# // - integer division with decimals discarded
+# % - division for module (remainder of the division)
+# / - division
+# * - multiplication
+# - - subtraction
+# + - addition
+print(23 % 7)
+print(2**5)
+print(25//4)
+
+# int() - conversion to integer
+# '' - string
+# float() - conversion to floating point number
+# variable declaration:
+my_name = 'DANIIL'
+my_surname = 'NIKIFOROV'
+my_age = 20
+print(my_name + ' ' + my_surname)
+print(my_name + ' ' + my_surname + ' ' + str(my_age))
+# easier:
+print(f"{my_name} {my_surname} {my_age}")
+print((my_name + " ") * 5)
+
+# Variable is an area memory at computer, which can storing single value
+color = 'green'
+
+print(color)
